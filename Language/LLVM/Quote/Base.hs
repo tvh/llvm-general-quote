5adbee57624d6b29c5ef2c11356f822e5815d8e5
Timo von Holtz <tvh@informatik.uni-kiel.de>
2014-03-21 15:27:18 +1100

    allow pattern quotation (not tested)

--- Language/LLVM/Quote/Base.hs
+++ Language/LLVM/Quote/Base.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
 
 module Language.LLVM.Quote.Base (
     ToDefintions(..),
@@ -18,10 +19,11 @@ import Data.Generics (extQ)
 import Data.Word
 import Data.Loc
 import Data.Typeable (Typeable)
-import Language.Haskell.Meta (parseExp)
+import Language.Haskell.Meta (parseExp, parsePat)
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote (QuasiQuoter(..),
-                                  dataToExpQ)
+                                  dataToExpQ,
+                                  dataToPatQ)
 
 import qualified Language.LLVM.Parser as P
 import qualified Language.LLVM.AST as A
@@ -340,6 +342,290 @@ qqExp = const Nothing `extQ` qqDefinitionE
                       `extQ` qqOperandE
                       `extQ` qqConstantE
 
+antiVarP :: String -> PatQ
+antiVarP = either fail return . parsePat
+
+qqDefinitionListP :: [A.Definition] -> Maybe (Q Pat)
+qqDefinitionListP [] = Just [p|[]|]
+qqDefinitionListP [A.AntiDefinitionList v] =
+    Just $ antiVarP v
+qqDefinitionListP (A.AntiDefinitionList _ : _ : _) =
+    error "Antiquoted list of definitions must be last item in quoted list"
+qqDefinitionListP (def : defs) =
+    Just [p|$(qqP def) : $(qqP defs)|]
+
+qqDefinitionP :: A.Definition -> Maybe (Q Pat)
+qqDefinitionP (A.GlobalDefinition v) =
+    Just [p|L.GlobalDefinition $(qqP v)|]
+qqDefinitionP (A.TypeDefinition n v) =
+    Just [p|L.TypeDefinition $(qqP n) $(qqP v)|]
+qqDefinitionP (A.MetadataNodeDefinition i vs) =
+    Just [p|L.MetadataNodeDefinition $(qqP i) $(qqP vs)|]
+qqDefinitionP (A.NamedMetadataDefinition i vs) =
+    Just [p|L.NamedMetadataDefinition $(qqP i) $(qqP vs)|]
+qqDefinitionP (A.ModuleInlineAssembly s) =
+    Just [p|L.ModuleInlineAssembly $(qqP s)|]
+qqDefinitionP a@(A.AntiDefinitionList _s) =
+    error $ "Internal Error: unexpected antiquote " ++ show a
+
+qqModuleP :: A.Module -> Maybe (Q Pat)
+qqModuleP (A.Module n dl tt ds) = 
+  Just [p|L.Module $(qqP n) $(qqP dl) $(qqP tt) $(qqP ds)|]
+
+qqGlobalP :: A.Global -> Maybe (Q Pat)
+qqGlobalP (A.GlobalVariable x1 x2 x3 x4 x5 x6 x7 x8 x9 xA xB) =
+  Just [p|L.GlobalVariable $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)
+                          $(qqP x6) $(qqP x7) $(qqP x8) $(qqP x9) $(qqP xA)
+                          $(qqP xB)|]
+qqGlobalP (A.GlobalAlias x1 x2 x3 x4 x5) =
+  Just [p|L.GlobalAlias $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)|]
+qqGlobalP (A.Function x1 x2 x3 x4 x5 x6 x7 x8 x9 xA xB xC) =
+  Just [p|L.Function $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)
+                    $(qqP x6) $(qqP x7) $(qqP x8) $(qqP x9) $(qqP xA)
+                    $(qqP xB) $(qqP xC)|]
+
+qqParameterP :: A.Parameter -> Maybe (Q Pat)
+qqParameterP (A.Parameter x1 x2 x3) =
+  Just [p|L.Parameter $(qqP x1) $(qqP x2) $(qqP x3)|]
+
+qqBasicBlockListP :: [A.BasicBlock] -> Maybe (Q Pat)
+qqBasicBlockListP [] = Just [p|[]|]
+qqBasicBlockListP [A.AntiBasicBlocks v] =
+    Just $ antiVarP v
+qqBasicBlockListP (A.AntiBasicBlocks v : defs) =
+    error "Antiquoted list of BasicBlocks must be last item in quoted list"
+qqBasicBlockListP (def : defs) =
+    Just [p|$(qqP def) : $(qqP defs)|]
+
+qqBasicBlockP :: A.BasicBlock -> Maybe (Q Pat)
+qqBasicBlockP (A.BasicBlock x1 x2 x3) =
+  Just [p|L.BasicBlock $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqBasicBlockP a@(A.AntiBasicBlocks _s) =
+  error $ "Internal Error: unexpected antiquote " ++ show a
+
+qqTerminatorP :: A.Terminator -> Maybe (Q Pat)
+qqTerminatorP (A.Ret x1 x2) =
+  Just [p|L.Ret $(qqP x1) $(qqP x2)|]
+qqTerminatorP (A.CondBr x1 x2 x3 x4) =
+  Just [p|L.CondBr $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqTerminatorP (A.Br x1 x2) =
+  Just [p|L.Br $(qqP x1) $(qqP x2)|]
+qqTerminatorP (A.Switch x1 x2 x3 x4) =
+  Just [p|L.Switch $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqTerminatorP (A.IndirectBr x1 x2 x3) =
+  Just [p|L.IndirectBr $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqTerminatorP (A.Invoke x1 x2 x3 x4 x5 x6 x7 x8) =
+  Just [p|L.Invoke $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)
+                  $(qqP x6) $(qqP x7) $(qqP x8)|]
+qqTerminatorP (A.Resume x1 x2) =
+  Just [p|L.Resume $(qqP x1) $(qqP x2)|]
+qqTerminatorP (A.Unreachable x1) =
+  Just [p|L.Unreachable $(qqP x1)|]
+
+qqMemoryOrderingP :: A.MemoryOrdering -> Maybe (Q Pat)
+qqMemoryOrderingP A.Unordered =
+  Just [p|L.Unordered|]
+qqMemoryOrderingP A.Monotonic =
+  Just [p|L.Monotonic|]
+qqMemoryOrderingP A.Acquire =
+  Just [p|L.Acquire|]
+qqMemoryOrderingP A.Release =
+  Just [p|L.Release|]
+qqMemoryOrderingP A.AcquireRelease =
+  Just [p|L.AcquireRelease|]
+qqMemoryOrderingP A.SequentiallyConsistent =
+  Just [p|L.SequentiallyConsistent|]
+
+qqAtomicityP :: A.Atomicity -> Maybe (Q Pat)
+qqAtomicityP (A.Atomicity x1 x2) =
+  Just [p|L.Atomicity $(qqP x1) $(qqP x2)|]
+
+qqLandingPadClauseP :: A.LandingPadClause -> Maybe (Q Pat)
+qqLandingPadClauseP (A.Catch x1) =
+  Just [p|L.Catch $(qqP x1)|]
+qqLandingPadClauseP (A.Filter x1) =
+  Just [p|L.Filter $(qqP x1)|]
+
+qqInstructionP :: A.Instruction -> Maybe (Q Pat)
+qqInstructionP (A.Add x1 x2 x3 x4 x5) =
+  Just [p|L.Add $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)|]
+qqInstructionP (A.FAdd x1 x2 x3) =
+  Just [p|L.FAdd $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.Sub x1 x2 x3 x4 x5) =
+  Just [p|L.Sub $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)|]
+qqInstructionP (A.FSub x1 x2 x3) =
+  Just [p|L.FSub $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.Mul x1 x2 x3 x4 x5) =
+  Just [p|L.Mul $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)|]
+qqInstructionP (A.FMul x1 x2 x3) =
+  Just [p|L.FMul $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.UDiv x1 x2 x3 x4) =
+  Just [p|L.UDiv $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqInstructionP (A.SDiv x1 x2 x3 x4) =
+  Just [p|L.SDiv $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqInstructionP (A.FDiv x1 x2 x3) =
+  Just [p|L.FDiv $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.URem x1 x2 x3) =
+  Just [p|L.URem $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.SRem x1 x2 x3) =
+  Just [p|L.SRem $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.FRem x1 x2 x3) =
+  Just [p|L.FRem $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.Shl x1 x2 x3 x4 x5) =
+  Just [p|L.Shl $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)|]
+qqInstructionP (A.LShr x1 x2 x3 x4) =
+  Just [p|L.LShr $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqInstructionP (A.AShr x1 x2 x3 x4) =
+  Just [p|L.AShr $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqInstructionP (A.And x1 x2 x3) =
+  Just [p|L.And $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.Or x1 x2 x3) =
+  Just [p|L.Or $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.Xor x1 x2 x3) =
+  Just [p|L.Xor $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.Alloca x1 x2 x3 x4) =
+  Just [p|L.Alloca $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqInstructionP (A.Load x1 x2 x3 x4 x5) =
+  Just [p|L.Load $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)|]
+qqInstructionP (A.Store x1 x2 x3 x4 x5 x6) =
+  Just [p|L.Store $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)
+                 $(qqP x6)|]
+qqInstructionP (A.GetElementPtr x1 x2 x3 x4) =
+  Just [p|L.GetElementPtr $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqInstructionP (A.Fence x1 x2) =
+  Just [p|L.Fence $(qqP x1) $(qqP x2)|]
+qqInstructionP (A.CmpXchg x1 x2 x3 x4 x5 x6) =
+  Just [p|L.CmpXchg $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)
+                  $(qqP x6)|]
+qqInstructionP (A.AtomicRMW x1 x2 x3 x4 x5 x6) =
+  Just [p|L.AtomicRMW $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)
+                     $(qqP x6)|]
+qqInstructionP (A.Trunc x1 x2 x3) =
+  Just [p|L.Trunc $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.ZExt x1 x2 x3) =
+  Just [p|L.ZExt $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.SExt x1 x2 x3) =
+  Just [p|L.SExt $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.FPToUI x1 x2 x3) =
+  Just [p|L.FPToUI $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.FPToSI x1 x2 x3) =
+  Just [p|L.FPToSI $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.UIToFP x1 x2 x3) =
+  Just [p|L.UIToFP $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.SIToFP x1 x2 x3) =
+  Just [p|L.SIToFP $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.FPTrunc x1 x2 x3) =
+  Just [p|L.FPTrunc $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.FPExt x1 x2 x3) =
+  Just [p|L.FPExt $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.PtrToInt x1 x2 x3) =
+  Just [p|L.PtrToInt $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.IntToPtr x1 x2 x3) =
+  Just [p|L.IntToPtr $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.BitCast x1 x2 x3) =
+  Just [p|L.BitCast $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.AddrSpaceCast x1 x2 x3) =
+  Just [p|L.AddrSpaceCast $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.ICmp x1 x2 x3 x4) =
+  Just [p|L.ICmp $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqInstructionP (A.FCmp x1 x2 x3 x4) =
+  Just [p|L.FCmp $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqInstructionP (A.Phi x1 x2 x3) =
+  Just [p|L.Phi $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.Call x1 x2 x3 x4 x5 x6 x7) =
+  Just [p|L.Call $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)
+                $(qqP x6) $(qqP x7)|]
+qqInstructionP (A.Select x1 x2 x3 x4) =
+  Just [p|L.Select $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqInstructionP (A.VAArg x1 x2 x3) =
+  Just [p|L.VAArg $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.ExtractElement x1 x2 x3) =
+  Just [p|L.ExtractElement $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.InsertElement x1 x2 x3 x4) =
+  Just [p|L.InsertElement $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqInstructionP (A.ShuffleVector x1 x2 x3 x4) =
+  Just [p|L.ShuffleVector $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqInstructionP (A.ExtractValue x1 x2 x3) =
+  Just [p|L.ExtractValue $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqInstructionP (A.InsertValue x1 x2 x3 x4) =
+  Just [p|L.InsertValue $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4)|]
+qqInstructionP (A.LandingPad x1 x2 x3 x4 x5) =
+  Just [p|L.LandingPad $(qqP x1) $(qqP x2) $(qqP x3) $(qqP x4) $(qqP x5)|]
+qqInstructionP (A.AntiInstruction s) =
+  Just $ antiVarP s
+
+qqNamedP :: (Typeable a, Data a) => A.Named a -> Maybe (Q Pat)
+qqNamedP ((A.:=) x1 x2) =
+  Just [p|(L.:=) $(qqP x1) $(qqP x2)|]
+qqNamedP (A.Do x1) =
+  Just [p|L.Do $(qqP x1)|]
+
+qqMetadataNodeIDP :: A.MetadataNodeID -> Maybe (Q Pat)
+qqMetadataNodeIDP (A.MetadataNodeID x1) =
+  Just [p|L.MetadataNodeID $(qqP x1)|]
+
+qqMetadataNodeP :: A.MetadataNode -> Maybe (Q Pat)
+qqMetadataNodeP (A.MetadataNode x1) =
+  Just [p|L.MetadataNode $(qqP x1)|]
+qqMetadataNodeP (A.MetadataNodeReference x1) =
+  Just [p|L.MetadataNodeReference $(qqP x1)|]
+
+qqOperandP :: A.Operand -> Maybe (Q Pat)
+qqOperandP (A.LocalReference x1) =
+  Just [p|L.LocalReference $(qqP x1)|]
+qqOperandP (A.ConstantOperand x1) =
+  Just [p|L.ConstantOperand $(qqP x1)|]
+qqOperandP (A.MetadataStringOperand x1) =
+  Just [p|L.MetadataStringOperand $(qqP x1)|]
+qqOperandP (A.MetadataNodeOperand x1) =
+  Just [p|L.MetadataNodeOperand $(qqP x1)|]
+
+qqConstantP :: A.Constant -> Maybe (Q Pat)
+qqConstantP (A.Int x1 x2) =
+  Just [p|L.Int $(qqP x1) $(qqP x2)|]
+qqConstantP (A.Float x1) =
+  Just [p|L.Float $(qqP x1)|]
+qqConstantP (A.Null x1) =
+  Just [p|L.Null $(qqP x1)|]
+qqConstantP (A.Struct x1 x2 x3) =
+  Just [p|L.Struct $(qqP x1) $(qqP x2) $(qqP x3)|]
+qqConstantP (A.Array x1 x2) =
+  Just [p|L.Array $(qqP x1) $(qqP x2)|]
+qqConstantP (A.Vector x1) =
+  Just [p|L.Vector $(qqP x1)|]
+qqConstantP (A.Undef x1) =
+  Just [p|L.Undef $(qqP x1)|]
+qqConstantP (A.BlockAddress x1 x2) =
+  Just [p|L.BlockAddress $(qqP x1) $(qqP x2)|]
+qqConstantP (A.GlobalReference x1) =
+  Just [p|L.GlobalReference $(qqP x1)|]
+qqConstantP (A.AntiConstant s) =
+  Just $ antiVarP s
+
+
+qqP :: Data a => a -> Q Pat
+qqP x = dataToPatQ qqPat x
+
+qqPat :: Typeable a => a -> Maybe (Q Pat)
+qqPat = const Nothing `extQ` qqDefinitionP
+                      `extQ` qqDefinitionListP
+                      `extQ` qqModuleP
+                      `extQ` qqGlobalP
+                      `extQ` qqParameterP
+                      `extQ` qqBasicBlockP
+                      `extQ` qqBasicBlockListP
+                      `extQ` qqTerminatorP
+                      `extQ` qqMemoryOrderingP
+                      `extQ` qqAtomicityP
+                      `extQ` qqLandingPadClauseP
+                      `extQ` qqInstructionP
+                      `extQ` (qqNamedP :: A.Named A.Instruction -> Maybe (Q Pat))
+                      `extQ` (qqNamedP :: A.Named A.Terminator -> Maybe (Q Pat))
+                      `extQ` qqMetadataNodeIDP
+                      `extQ` qqMetadataNodeP
+                      `extQ` qqOperandP
+                      `extQ` qqConstantP
+
 parse :: [A.Extensions]
       -> P.P a
       -> String
@@ -361,8 +647,8 @@ quasiquote :: Data a
            -> P.P a
            -> QuasiQuoter
 quasiquote exts p =
-    QuasiQuoter { quoteExp  = parse exts p >=> dataToExpQ qqExp
-                , quotePat  = fail "LLVM pattern quasiquoter undefined"
+    QuasiQuoter { quoteExp  = parse exts p >=> qqE
+                , quotePat  = parse exts p >=> qqP
                 , quoteType = fail "LLVM type quasiquoter undefined"
                 , quoteDec  = fail "LLVM declaration quasiquoter undefined"
                 }
