58c225b5048aad089d2023a4dd3f5a33c5fb9287
Timo von Holtz <tvh@informatik.uni-kiel.de>
2014-03-31 11:10:57 +1100

    add skeleton for tests

---
+++ test/Language/LLVM/Test/Tests.hs
@@ -0,0 +1,5 @@
+module Language.LLVM.Test.Tests where
+
+import Test.Tasty
+
+tests = testGroup "language-llvm-quote" []
---
+++ test/test.hs
@@ -0,0 +1,4 @@
+import Test.Tasty
+import qualified Language.LLVM.Test.Tests as Language.LLVM
+
+main = defaultMain Language.LLVM.tests
